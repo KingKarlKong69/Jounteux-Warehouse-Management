b24b9de0c32bb3d4478420576541f2b4cad020d1
KingKarlKong69 <lopezkristine02@gmail.com>
2026-06-23 22:38:21 +0800

    Fix trusted proxy header constants

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -24,7 +24,12 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->trustProxies(
             at: '*',
-            headers: Request::HEADER_X_FORWARDED_ALL
+            headers: Request::HEADER_FORWARDED
+                | Request::HEADER_X_FORWARDED_FOR
+                | Request::HEADER_X_FORWARDED_HOST
+                | Request::HEADER_X_FORWARDED_PROTO
+                | Request::HEADER_X_FORWARDED_PORT
+                | Request::HEADER_X_FORWARDED_PREFIX
         );
 
         $middleware->web(append: [
