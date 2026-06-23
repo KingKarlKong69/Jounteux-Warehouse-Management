d8286ae1ce0aa7fd1034b5906206e7374827ff99
KingKarlKong69 <lopezkristine02@gmail.com>
2026-06-23 10:44:15 +0800

    Fix Echo broadcaster config for Reverb

--- resources/js/echo.ts
+++ resources/js/echo.ts
@@ -20,7 +20,7 @@ import Pusher from 'pusher-js'
 ;(window as any).Pusher = Pusher
 
 const echo = new Echo({
-    broadcaster: 'reverb',
+    broadcaster: 'pusher',
     key: import.meta.env.VITE_REVERB_APP_KEY,
     wsHost: import.meta.env.VITE_REVERB_HOST,
     wsPort: Number(import.meta.env.VITE_REVERB_PORT ?? 8080),
